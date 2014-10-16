0114213ada7d64925b31326dc91b2631c864e13f
dehuaz <dezheng6@gmail.com>
2014-10-16 09:23:00 -0700

    JavaScript Ex

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@
 	Javascript 
 	Comment
 */
+
+var person = {
+	fName: 'Daniel',
+	lName: 'Zheng',
+	email: 'dehuaz@uw.edu',
+	getFullName: function() { return this.fName + ' ' + this.lName; }
+};
+
+console.log(person.getFullName());
